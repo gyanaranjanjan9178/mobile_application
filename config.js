827972853904575aeb5e92a6faedab2c01854b34
gyanaranjanjan9178 <158822201+gyanaranjanjan9178@users.noreply.github.com>
2025-07-18 01:17:31 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,25 @@
+
+module.exports = {
+  development: {
+    username: 'postgres',
+    password: 'Abhimanyau9178',
+    database: 'catalog_app',
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  },
+  test: {
+    username: 'postgres',
+    password: 'Abhimanyau9178',
+    database: 'catalog_app_test',
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  },
+  production: {
+    username: 'postgres',
+    password: 'Abhimanyau9178',
+    database: 'catalog_app_prod',
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  }
+};
+
